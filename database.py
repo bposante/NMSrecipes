d04fbd88bdbb275ed4c1544a85b4fcf7dd145632
Bailey Posante <baileyposante@gmail.com>
2020-10-04 18:24:10 -0400

    connects to database; creates if one does not exist

--- database.py
+++ database.py
@@ -1,13 +1,28 @@
 import mysql.connector
 import db_settings as dbs
 
-mydb = mysql.connector.connect(
-  host=dbs.host,
-  user=dbs.username,
-  password=dbs.password
-)
+class NMSDatabase:
+    def __init__(self):
+        self.mydb = None
+        self.mycursor = None
+        try:
+            print("Attempting to connect to database")
+            self.mydb = mysql.connector.connect(
+                host=dbs.host,
+                user=dbs.username,
+                password=dbs.password,
+                database="NMS_data"
+            )
+        except mysql.connector.errors.ProgrammingError:
+            print("Database not found, creating database")
+            self.mydb = mysql.connector.connect(
+                host=dbs.host,
+                user=dbs.username,
+                password=dbs.password
+            )
+            mycursor = self.mydb.cursor()
+            mycursor.execute("CREATE DATABASE NMS_data")
+        print("Successfully connected to database.")
 
-mycursor = mydb.cursor()
-
-#mycursor.execute("DROP DATABASE mydatabase")
-mycursor.execute("CREATE DATABASE mydatabase")
+if __name__ == "__main__":
+    test = NMSDatabase()
